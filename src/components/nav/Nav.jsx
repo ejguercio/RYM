94a9ae88836d8b08b94daca310bd0bb2131907bd
ejguercio <jguercio@live.com.ar>
2023-04-20 09:59:56 -0300

    antes de aplicar estados

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBar from "../search-bar/SearchBar";
+
+const Nav = () => {
+
+
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+};
+
+export default Nav;
